9cfa56d4afd35801bdc981771964dd83de54de37
mohamedraguig <raguigmed@gmail.com>
2022-08-29 15:35:24 +0200

    allow cross origin requests from frontend application

---
+++ src/main/java/fr/pizzahut/pizzahutrhms/WebConfig.java
@@ -0,0 +1,22 @@
+package fr.pizzahut.pizzahutrhms;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://ph-rh-frontend.herokuapp.com")
+                .allowedMethods("*")
+                .maxAge(3600L)
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
+    }
+}
